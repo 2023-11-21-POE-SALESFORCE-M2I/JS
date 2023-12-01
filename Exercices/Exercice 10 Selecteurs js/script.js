6ed9ce7c3af87c55e7a67fc5680f6b7df313fc63
AudreyD <a@a.a>
2023-12-01 11:38:45 +0100

    Énoncé exercice 10 - sélecteur JS

---
+++ Exercices/Exercice 10 Selecteurs js/script.js
@@ -0,0 +1,7 @@
+/*
+    Exercice 10 :
+
+    1) Sélectionner l'élément ayant la classe ".start" dans le deuxième container
+
+    2) Sélectionner le 4ème "li" du premier container à partir de l'élément ".start", puis l'afficher dans la console.
+*/
