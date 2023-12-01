73804543a9072a10174c67923b26ba905fff5fdb
AudreyD <a@a.a>
2023-12-01 15:54:35 +0100

    Correction exercice 10 - sélecteurs JS

--- Exercices/Exercice 10 Selecteurs js/script.js
+++ Exercices/Exercice 10 Selecteurs js/script.js
@@ -5,3 +5,18 @@
 
     2) Sélectionner le 4ème "li" du premier container à partir de l'élément ".start", puis l'afficher dans la console.
 */
+
+// 1) Selectionner l'élement ".start"
+let startElement = document.querySelector('li.start');
+
+// 2) Selectionner l'élément à selectionner à partir de l'élément ".start"
+let target = startElement.parentElement.parentElement.parentElement.previousElementSibling.children[2].children[3];
+// Selection du parent de notre startElement
+// Puis Sélection de la div parent section
+// Puis Sélection de la div parent container
+// Puis Sélection du frère avant lui
+// Puis Sélection du 3em enfant qui se trouve dans le 1er container
+// Enfin Selection du 4eme li
+
+// 3) Afficher l'élément sélectionné dans la console
+console.log(target);
